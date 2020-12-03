738beedccca47939c44f0026597ac39a408ec859
Lucas Sônego <sonego_lucas@outlook.com>
2020-12-02 22:23:01 -0300

    generate initial state

---
+++ index.js
@@ -0,0 +1,19 @@
+function generateInitialState(size) {
+  let state = [];
+
+  for (let line = 0; line < size; line++) {
+    let currentLine = [];
+    for (let column = 0; column < size; column++) {
+      if (line < size - 1) {
+        currentLine.push(0);
+      } else {
+        currentLine.push(1);
+      }
+    }
+    state.push(currentLine);
+  }
+  return state;
+}
+
+const size = 4;
+const initialState = generateInitialState(size);
